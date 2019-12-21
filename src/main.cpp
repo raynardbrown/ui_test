ac5ba01d483ba94befc97ce30b580b050e04d3e6
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-21 17:10:33 -0600

    feat: Add the main source

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: main.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
